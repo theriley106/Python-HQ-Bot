c9f833e68c3f7a4e1fcaf32827c08f0b04d1c0f4
Theriley106 <christopherlambert106@gmail.com>
2018-05-06 15:57:05 -0400

    Added clean DB generator

--- cleanDB.py
+++ cleanDB.py
@@ -2,7 +2,11 @@
 # encoding: utf-8
 import csv
 import json
+import re
 
+letters = ["A", "B", "C", "D"]
+
+DATABASE = []
 def readCSV(csvFile):
 	with open(csvFile, 'r') as f:
  		reader = csv.reader(f)
@@ -10,8 +14,19 @@ def readCSV(csvFile):
 listOfAnswers = readCSV('HQTriviaScribe_tweets.csv')
 
 for val in listOfAnswers:
+	tempDict = {}
+	tempDict["timestamp"] = val[0]
 	tempVals = val[2].split("\n")
-	question = tempVals[0]
-	if '✓' in str(val[2]):
-		print 'check'
-	print str(val[2])
+	tempDict["question"] = tempVals[0]
+	tempDict["answers"] = []
+	for i, val in enumerate(tempVals[1:]):
+		nestDict = {}
+		nestDict["choice"] = letters[i]
+		if '✓' in str(val):
+			nestDict["correct"] = True
+		else:
+			nestDict["correct"] = False
+		nestDict["text"] = val.replace(" ✓", "").partition(") ")[2]
+		tempDict["answers"].append(nestDict)
+	DATABASE.append(tempDict)
+print DATABASE
