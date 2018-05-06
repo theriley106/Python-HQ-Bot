979e3b136f5deb6d827a31613aef260673b32886
Theriley106 <christopherlambert106@gmail.com>
2018-05-06 15:36:29 -0400

    Added git ignore

--- genDB.py
+++ genDB.py
@@ -61,4 +61,4 @@ def get_all_tweets(screen_name):
 
 if __name__ == '__main__':
 	#pass in the username of the account you want to download
-	get_all_tweets("J_tsar")
+	get_all_tweets("HQTriviaScribe")
